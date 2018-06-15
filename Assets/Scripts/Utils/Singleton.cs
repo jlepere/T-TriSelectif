b5224dc227276f350daf362f136b7a2814158d2a
Jason Lepère <jlepere.rg@gmail.com>
2018-06-15 22:04:46 +0200

    Initial commit

---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+  protected static T instance;
+
+  protected virtual void Awake()
+  {
+    instance = this as T;
+  }
+
+  public static T Instance
+  {
+    get
+    {
+      if (instance == null && (instance = FindObjectOfType<T>()) == null)
+      {
+        GameObject newInstance = new GameObject();
+        instance = newInstance.AddComponent<T>();
+      }
+      return instance;
+    }
+  }
+}
